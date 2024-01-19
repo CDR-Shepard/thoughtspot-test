3de5ccbe8e8ad8f7b1207d5ed97ff012e609c569
CDR-Shepard <56300999+CDR-Shepard@users.noreply.github.com>
2024-01-19 09:47:18 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,103 +1,37 @@
-// Import ThoughtSpot SDK
+// Import the LiveboardEmbed SDK
 import {
-  init,
-  LiveboardEmbed,
-  Action,
-  RuntimeFilterOp,
-  EmbedEvent,
-  AuthType,
-  HostEvent
-} from "@thoughtspot/visual-embed-sdk";
-import "./styles.css";
+    LiveboardEmbed,
+    AuthType,
+    init,
+    prefetch,
+    EmbedEvent
+} from 'https://cdn.jsdelivr.net/npm/@thoughtspot/visual-embed-sdk/dist/tsembed.es.js';
 
-// Initialize embed configuration
+// Initialize the SDK
 init({
-  thoughtSpotHost:
-    /*param-start-hosturl*/"https://team1.thoughtspot.cloud"/*param-end-hosturl*/,
-  authType: AuthType.None,
-  /*param-start-styleCustomization*//*param-end-styleCustomization*/
+    thoughtSpotHost: "https://[your-thoughtspot-host]",
+    authType: AuthType.None
 });
 
-// Instantiate class for embedding a Liveboard
-const embed = new LiveboardEmbed("#your-own-div", {
-    frameParams: {},
-    /*param-start-liveboardFullHeight*/
-     fullHeight: true,
-/*param-end-liveboardFullHeight*/
-    /*param-start-modifyActions*//*param-end-modifyActions*/
-    /*param-start-liveboardId*/
-     liveboardId: "55d08cfe-263e-4e65-a36c-f89f19cd675a",
-/*param-end-liveboardId*/
-    /*param-start-activeTabId*//*param-end-activeTabId*/
-    /*param-start-runtimeFilters*//*param-end-runtimeFilters*/
+// Create an instance of the LiveboardEmbed class
+const liveboardEmbed = new LiveboardEmbed(document.getElementById('ts-embed'), {
+    frameParams: {
+        width: '100%',
+        height: '100%',
+    },
+    liveboardId: "55d08cfe-263e-4e65-a36c-f89f19cd675a",
+    fullHeight: true,
+    visibleVizs: [],
+    runtimeFilters: [{
+        columnName: '<column-name>',
+        operator: '<RuntimeFilterOp>',
+        values: ['value']
+    }],
 });
 
-hideNoDataImage();
-showErrorBanner('none');
+// Register event listeners
+liveboardEmbed.on(EmbedEvent.Init, () => console.log('Liveboard init'));
+liveboardEmbed.on(EmbedEvent.Load, () => console.log('Liveboard loaded'));
 
-embed
-    // Register event listeners
-    .on(EmbedEvent.Init, showLoader)
-    .on(EmbedEvent.Load, hideLoader)
-    .on(EmbedEvent.AuthExpire, showAuthExpired)
-    /*param-start-customActionHandle*//*param-end-customActionHandle*/
-    .on(EmbedEvent.Error, (error) => {
-        if(error?.data?.errorType === 'FULLSCREEN') {
-          showErrorBanner('none');
-        } else 
-        if(typeof(error.error) === 'string') {
-          showErrorBanner('flex', error.error);
-        } else {
-          showErrorBanner('flex');
-        }
-        console.log('Error ', error);
-        hideLoader();
-    })
-    // Render Liveboard
-    .render();
-
-/*param-start-useHostEvent*/
-/*param-end-useHostEvent*/
-
-// Functions to show/hide
-function setDisplayStyle(el, style) {
-  if(document.getElementById(el)) {
-    document.getElementById(el).style.display = style;
-  }
-}
-
-// Functions to show and hide a loader while iframe loads
-function showLoader() {
-  setDisplayStyle("loader", "block");
-}
-function hideLoader() {
-  setDisplayStyle("loader", "none");
-}
-
-function showAuthExpired() {
-  setDisplayStyle("authExpiredBanner", "flex");
-}
-
-// Functions to show or hide No data images
-function showNoDataImage() {
-  setDisplayStyle("no-data", "block");
-}
-
-function hideNoDataImage() {
-  setDisplayStyle("no-data", "none");
-}
-
-function showErrorBanner(display, errorText) {
-  setDisplayStyle("errorBanner", display);
-  if(errorText) {
-    document.getElementById("errorBanner").firstElementChild.innerText = errorText;
-  }
-}
-
-document.getElementById('authExpiredBannerCloseBtn').addEventListener('click', () => {
-  setDisplayStyle("authExpiredBanner", "none");
-});
-
-document.getElementById('errorBannerCloseBtn').addEventListener('click', () => {
-  setDisplayStyle("errorBanner", "none");
-});
+// Render the embedded Liveboard
+liveboardEmbed.render();
